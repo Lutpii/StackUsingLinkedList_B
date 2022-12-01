5d97522762a56fcfc3efc9779dfbb5b9d7aae371
Adi Luthfi Nur Roki <adiluthfi03@gmail.com>
2022-12-01 16:21:43 +0700

    add class node

--- StackUsingLinkedList_B/Program.cs
+++ StackUsingLinkedList_B/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StackUsingLinkedList_B
 {
+    class Node
+    {
+        public int info;
+        public Node next;
+        public Node(int i, Node n)
+        {
+            info = i;
+            next = n;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
