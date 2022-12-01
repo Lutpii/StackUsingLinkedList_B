548db2eb842f3dda9b58e29fbcfc41b10ebccd7e
Adi Luthfi Nur Roki <adiluthfi03@gmail.com>
2022-12-01 16:33:39 +0700

    add method pop

--- StackUsingLinkedList_B/Program.cs
+++ StackUsingLinkedList_B/Program.cs
@@ -39,6 +39,12 @@ namespace StackUsingLinkedList_B
             top = fresh;
             Console.WriteLine("\n" + element + " pushed.");
         }
+
+        public void pop()
+        {
+            Console.WriteLine("\nThe popped element is: " + top.info);
+            top = top.next; //Make top point to the next node in sequence
+        }
     }
     internal class Program
     {
